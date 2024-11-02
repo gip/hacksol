482757d2680dfb85107533db5d1c0e94992b826a
Gilles Pirio <g36130@gmail.com>
2024-11-01 20:58:38 -0700

    feat: auth

--- web/components/Workbench/Workbench.tsx
+++ web/components/Workbench/Workbench.tsx
@@ -1,8 +1,28 @@
 import { Anchor, Text, Title } from '@mantine/core';
 import classes from './Welcome.module.css';
+import { useEffect } from 'react';
+import { useJsonSigning } from '@/lib/client/useJsonSigning';
 
 export function Workbench({ connected, publicKey }: any) {
+    const { signJson, isReady } = useJsonSigning();
 
+    useEffect(() => {
+        if(!isReady || !publicKey) {
+            console.log('NRR Not ready');
+        };
+        const f = async () => {
+            const payload = await signJson({});
+            const r = await fetch("/api/auth", {
+                method: 'POST',
+                body: JSON.stringify({
+                    user: publicKey.toBase58(),
+                    payload
+                })
+            });
+            console.log('RRR', r);
+        };
+        f();
+    }, [publicKey]);
 
     return (
     <>
